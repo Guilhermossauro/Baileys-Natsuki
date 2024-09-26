e54fd7eb42669dedc3b6b70ce09019c4cf62eb2f
Guilherme Augusto Ferreira <56206350+Guilhermossauro@users.noreply.github.com>
2024-09-26 12:24:47 -0300

    Update tts.js
    corrigindo para que funcione com o baileys, codigo original era feito baseado no open wa

--- modules/geral/tts.js
+++ modules/geral/tts.js
@@ -4,6 +4,7 @@ const urlParse = require('url').parse;
 const http = require('http');
 const https = require('https');
 const fs = require('fs-extra');
+const { responderTexto } = require('../../sockets');
 
 async function downloadFile(url, dest) {
     return new Promise((resolve, reject) => {
@@ -19,19 +20,14 @@ async function downloadFile(url, dest) {
 
         httpClient
             .get(options, (res) => {
-                // check status code
                 if (res.statusCode !== 200) {
                     const msg = `request to ${url} failed, status code = ${res.statusCode} (${res.statusMessage})`;
                     reject(new Error(msg));
                     return;
                 }
                 const file = fs.createWriteStream(dest);
-                file.on('finish', function () {
-                    // close() is async, call resolve after close completes.
-                    file.close(resolve);
-                });
-                file.on('error', function (err) {
-                    // Delete the file async. (But we don't check the result)
+                file.on('finish', () => file.close(resolve));
+                file.on('error', (err) => {
                     fs.unlink(dest);
                     reject(err);
                 });
@@ -43,31 +39,29 @@ async function downloadFile(url, dest) {
 }
 
 exports.tts = async function tts(client, message) {
-    const { id, from, body, chat, caption, isGroupMsg} = message;
-
-    if (!isGroupMsg) {
-        await client.react(id, "🤷🏻‍♂️");
-         client.reply(from, "Este comando só pode ser usado em grupos.", id);
-         return
+    const { id, from, mimetyped, chat, caption, isGroup } = message;
+    const mensagem =  mimetyped.command
+    const commands = mensagem
+    if (!isGroup) {
+        await responderTexto(client, from, "Este comando só pode ser usado em grupos.", message);
+        return;
+    }
+    const args = commands.split(" ");
+    if (args.length === 1) {
+        return responderTexto(client, from, 'Foi mau, mas assim eu não consigo... preciso saber de uma frase também!', message);
     }
 
-    const commands = caption || body || "";
-    const args = commands.split(" ");
-    const grupo= chat.id
-   
-
-    if (args.length === 1) return client.reply(from, 'Foi mau, mas assim eu não consigo... preciso saber de uma frase também!', id);
     let string = commands.split(' ').slice(1).join(' ');
     if (string.length >= 200) {
-        return client.reply(from, `MIZERICORDIA  bisho q treco grande, quer me bugar??`, id);
+        return responderTexto(client, from, `MIZERICORDIA bisho q treco grande, quer me bugar??`, message);
     }
-    const url = googleTTS.getAudioUrl(`${string}`, {
+    const url = googleTTS.getAudioUrl(string, {
         lang: 'pt_BR',
-        slow: true,
+        slow: false,
         host: 'https://translate.google.com',
     });
-
-    const dest = await path.resolve(__dirname, '../media/translate.mp3');
+    const dest = await path.resolve(__dirname, '../../media/translate.mp3');
     await downloadFile(url, dest);
-    await client.sendPtt(from, dest, 'translate', 'AAAAAAAAAUHHH', id);
-}
+    await client.sendMessage(from, { audio: fs.readFileSync(dest),mimetype:'audio/mpeg', ptt: true }, { quoted: message });
+     await fs.unlink(dest);
+};
